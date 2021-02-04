7fa1066b7f049bfc6443f0ff353e94dbad4755bb
Sabia Tahir <sabiatahir@hotmail.com>
2021-02-04 23:56:39 +0000

    Update function to use lightbnb database and SQL

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -98,8 +98,7 @@ const getAllProperties = function(options, limit = 10) {
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id `;
-
+  LEFT JOIN property_reviews ON properties.id = property_id `;
 
   if (options.city || options.owner_id || options.minimum_price_per_night || options.maximum_price_per_night) {
     queryString += `WHERE `;
@@ -157,10 +156,19 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+
+  const queryString = `
+  INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `;
+
+  const values = [property.owner_id, `${property.title}`, `${property.description}`, `${property.thumbnail_photo_url}`, `${property.cover_photo_url}`, `${property.cost_per_night}`, `${property.street}`, `${property.city}`, `${property.province}`, `${property.post_code}`, `${property.country}`, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms];
+
+  return pool.query(queryString, values)
+  .then(res => res.rows[0])
+  .catch(error => console.log(error))
+
 }
 exports.addProperty = addProperty;
 
